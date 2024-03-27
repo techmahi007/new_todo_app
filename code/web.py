40057ae8fcade1664a37f378247c85bea672cdad
techmahi007 <mahiprogram29@gmail.com>
2024-03-27 15:36:59 +0530

    organized the files

---
+++ code/web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"].title() + '\n'
+    todos.append(new_todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+st.title("My To-Do App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    # To remove the completed tasks
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
